56465241214d8d84051823cd72af5cd012ca615e
Travis Reeder <treeder@gmail.com>
2014-01-20 12:56:15 -0800

    Update usersetup.rb

--- usersetup.rb
+++ usersetup.rb
@@ -3,3 +3,5 @@
 # require 'mongo'
 # db = Mongo::MongoClient.from_uri("mongodb://user:pass@x.mongolab.com:12345/tester")
 # IronConsumer.connections['mymongo'] = db.db()
+
+# todo: put a bunch of examples connecting to various databases / services in here. 
